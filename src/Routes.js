fd1e6fbc321fb8ce3de55c4fe8a8c3c736cceecb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-06 20:57:11 +0200

    delete unnecessary changes

--- src/Routes.js
+++ src/Routes.js
@@ -2,18 +2,15 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import WithLayout from 'WithLayout';
 // Available layouts
-import { Main as MainLayout, Fluid as FluidLayout } from './layouts';
+import { Main as MainLayout } from './layouts';
 
 // Landing pages
-import { Home as HomeView, Crypto as CryptoView } from './views/landingPages';
+import { Crypto as CryptoView } from './views/landingPages';
 
 // Authentication pages
 import {
-  Login as LoginView,
   LoginSimple as LoginSimpleView,
-  Signup as SignupView,
   SignupSimple as SignupSimpleView,
-  ForgotPassword as ForgotPasswordView,
   ForgotPasswordSimple as ForgotPasswordSimpleView,
 } from './views/authPages';
 
@@ -26,30 +23,8 @@ const Routes = () => {
         render={(matchProps) => (
           <WithLayout
             {...matchProps}
-            component={HomeView}
-            layout={MainLayout}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/landing-crypto"
-        render={(matchProps) => (
-          <WithLayout
-            {...matchProps}
             component={CryptoView}
             layout={MainLayout}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/page-login"
-        render={(matchProps) => (
-          <WithLayout
-            {...matchProps}
-            component={LoginView}
-            layout={FluidLayout}
           />
         )}
       />
@@ -66,34 +41,12 @@ const Routes = () => {
       />
       <Route
         exact
-        path="/page-signup"
-        render={(matchProps) => (
-          <WithLayout
-            {...matchProps}
-            component={SignupView}
-            layout={FluidLayout}
-          />
-        )}
-      />
-      <Route
-        exact
         path="/page-signup-simple"
         render={(matchProps) => (
           <WithLayout
             {...matchProps}
             component={SignupSimpleView}
             layout={MainLayout}
-          />
-        )}
-      />
-      <Route
-        exact
-        path="/page-forgot-password"
-        render={(matchProps) => (
-          <WithLayout
-            {...matchProps}
-            component={ForgotPasswordView}
-            layout={FluidLayout}
           />
         )}
       />
