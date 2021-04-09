8b5035ecbfa209bf7e4974353fcc54226ec2c762
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-09 19:09:59 +0200

    show uploaded image

--- src/views/dashboard/createNFT/createNFT.js
+++ src/views/dashboard/createNFT/createNFT.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 // dependency for validation: https://www.npmjs.com/package/react-images-upload
 const CreateNFT = () => {
   const [images, setImages] = useState([]);
+  const [uploadedImage, setUploadedImage] = useState(null);
 
   const onDrop = (picture) => {
     console.log('drop', picture);
@@ -14,31 +15,47 @@ const CreateNFT = () => {
   //   console.log('yeah', file);
   // };
 
-  const uploadImages = () => {
-    const uploadImagesPromise = images.map((image) => {
-      const data = new FormData();
-      data.append('image', image, image.name);
-      return axios.post('http://localhost:5000/nft/upload', data);
-    });
+  const uploadImages = async () => {
+    const formData = new FormData();
+    formData.append('image', images[0]);
 
-    axios
-      .all(uploadImagesPromise)
-      .then((res) => console.log(res))
-      .catch((error) => console.log(error));
+    const result = await axios.post(
+      'http://localhost:5000/nft/upload',
+      formData,
+      { headers: { 'Content-Type': 'multipart/form-data' } },
+    );
+
+    setUploadedImage(result.data.imagePath);
   };
 
   return (
-    <div>
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <ImageUploader
         withIcon={true}
         buttonText="Choose images"
         onChange={onDrop}
         imgExtension={['.jpg', 'png', '.gif', '.gif']}
         maxFileSize={5242880}
-        // onChange={yeah}
         withPreview={true}
       />
       <button onClick={uploadImages}>Upload maaaaan</button>
+      {uploadedImage && (
+        <div style={{ width: '200px', height: '200px' }}>
+          <img
+            style={{ width: '100%', height: '100%' }}
+            src={`http://localhost:5000/nft/${uploadedImage}`}
+            alt="just a test"
+          />
+        </div>
+      )}
     </div>
   );
 };
