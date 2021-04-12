4f44ef14202ad12e1b95a6891f1b8af6eb4b2e87
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-12 19:39:16 +0200

    change tokenomics

--- src/views/landingPage/Crypto/components/Download/Download.js
+++ src/views/landingPage/Crypto/components/Download/Download.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Chart } from 'react-google-charts';
 
 const Download = () => {
   const theme = useTheme();
-  const isSm = useMediaQuery(theme.breakpoints.up('sm'), {
-    defaultMatches: true,
-  });
 
   return (
     <Box>
@@ -33,13 +28,13 @@ const Download = () => {
           align={'center'}
           gutterBottom
         >
-          Team tokens will be locked for multiple months.
+          Team tokens will be locked for 3 months.
         </Box>
         <Box align={'center'}>
           <Chart
             width={'100%'}
             height={'300px'}
-            chartType="PieChart"
+            chartType='PieChart'
             loader={<div>Loading Chart</div>}
             data={[
               ['Reason', 'Percentage of token supply'],
@@ -62,38 +57,9 @@ const Download = () => {
           color={'textSecondary'}
           align={'center'}
         >
-          Depending on the amount we collect during the presale, over 50% of the
-          collected funds will go into the liquidty pool, locked for over 1
-          year. Rest of the funds will go into marketing and artists to boost
-          the activity on our NFT marketplace.
+          50% of the collected funds will go into the liquidty pool, locked for
+          over 1 year. The Rest of the funds will go into marketing.
         </Typography>
-      </Box>
-      <Box
-        display="flex"
-        flexDirection={{ xs: 'column', sm: 'row' }}
-        alignItems={{ xs: 'stretched', sm: 'flex-start' }}
-        justifyContent={'center'}
-      >
-        <Box
-          component={Button}
-          variant="outlined"
-          color="primary"
-          size="large"
-          fullWidth={isSm ? false : true}
-        >
-          Contact sales
-        </Box>
-        <Box
-          component={Button}
-          variant="contained"
-          color="primary"
-          size="large"
-          fullWidth={isSm ? false : true}
-          marginTop={{ xs: 1, sm: 0 }}
-          marginLeft={{ sm: 2 }}
-        >
-          Download
-        </Box>
       </Box>
     </Box>
   );
