859e83d3027e39777ae1a0bffe6d59948553469a
Liam Sutton <devliam90@gmail.com>
2021-04-21 23:56:53 +0100

    header animations

--- src/views/LandingPage-2/components/Header/Header.js
+++ src/views/LandingPage-2/components/Header/Header.js
@@ -5,9 +5,9 @@ const Header = () => {
     <header className='header'>
       <div className='header__content container'>
         <div className='header__text-box'>
-          <h1 className='heading-primary'>
-            NFTChain
-            <br />A revolutionary marketplace
+          <h1 className='heading-primary heading-primary-1'>NFTChain</h1>
+          <h1 className='heading-primary heading-primary-2'>
+            A revolutionary marketplace
           </h1>
           <p className='heading-primary-main'>
             NFTChain is a centralized network for collectors who can turn their
