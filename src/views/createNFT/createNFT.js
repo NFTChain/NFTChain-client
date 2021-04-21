fcca51ab48fd07657f9f296cf2233088fa022151
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-21 14:49:07 +0200

    specify upload route

--- src/views/createNFT/createNFT.js
+++ src/views/createNFT/createNFT.js
@@ -77,11 +77,15 @@ const CreateNFT = ({
       const metadata = JSON.stringify(fileMetaDataObject);
       data.append('pinataMetadata', metadata);
 
-      const result = await axios.post('https://nftchain.herokuapp.com/', data, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
+      const result = await axios.post(
+        'https://nftchain.herokuapp.com/nft/upload/',
+        data,
+        {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        },
+      );
 
-      if (result?.message === 'upload successful') {
+      if (result.data.message === 'upload successful') {
         await mintNFTTokenForUploadedFile(result.data.ipfs_hash);
       }
     } else {
