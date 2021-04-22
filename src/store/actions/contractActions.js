f110453bac51c8062e33b5cccd5063d607a96c1e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-22 13:22:23 +0200

    add try catch block to contract actions

--- src/store/actions/contractActions.js
+++ src/store/actions/contractActions.js
@@ -6,22 +6,28 @@ import {
 } from '../../utils/getContract';
 
 export const connectToContract = (contractType) => async (dispatch) => {
-  // get the smart contract to be able to communicate with the blockchain
-  const { signerAddress, token } = await getContract(contractType);
-  dispatch({
-    type: actionTypes.GET_CONTRACT,
-    payload: {
-      contractType,
-      signerAddress,
-      token,
-    },
-  });
-  // If BEP20 or ERC721 token => get the balance from users wallet
-  if (
-    BEP20ContractString === contractType ||
-    BEP721ContractString === contractType
-  ) {
-    return dispatch(getContractBalance(contractType, token, signerAddress));
+  try {
+    // get the smart contract to be able to communicate with the blockchain
+    const { signerAddress, token } = await getContract(contractType);
+    dispatch({
+      type: actionTypes.GET_CONTRACT,
+      payload: {
+        contractType,
+        signerAddress,
+        token,
+      },
+    });
+    // If BEP20 or ERC721 token => get the balance from users wallet
+    if (
+      BEP20ContractString === contractType ||
+      BEP721ContractString === contractType
+    ) {
+      return dispatch(getContractBalance(contractType, token, signerAddress));
+    }
+  } catch (error) {
+    debugger;
+    console.log(error);
+    // dispatch error message here
   }
 };
 
@@ -31,13 +37,19 @@ export const getContractBalance = (
   signerAddress,
 ) => async (dispatch) => {
   // get users balance of tokens
-  let balance = (await contract.balanceOf(signerAddress)).toString();
+  try {
+    const balance = (await contract.balanceOf(signerAddress)).toString();
 
-  dispatch({
-    type: actionTypes.GET_CONTRACT_BALANCE,
-    payload: {
-      contractType,
-      balance,
-    },
-  });
+    dispatch({
+      type: actionTypes.GET_CONTRACT_BALANCE,
+      payload: {
+        contractType,
+        balance,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    debugger;
+    // dispatch error message here
+  }
 };
