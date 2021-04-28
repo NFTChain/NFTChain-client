ff799c18ecc63b80bc964256be0f91165463fbd4
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-28 15:11:08 +0200

    add material icons and left side of nav bar

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -3,9 +3,9 @@ import Header from './components/Header';
 import Ecosystem from './components/Ecosystem';
 import Roadmap from './components/Roadmap';
 import Features from './components/Features';
-import Nav from '../Layout/components/Nav';
+import Nav from './components/Nav';
 import Socials from './components/Socials';
-import Footer from '../Layout/components/Footer';
+import Footer from './components/Footer';
 
 const LandingPage = () => {
   return (
--- src/views/Layout/components/Nav/Nav.js
+++ src/views/Layout/components/Nav/Nav.js
@@ -1,41 +1,65 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../assets/onlyLogo.png';
+import { H3, H6 } from '../../../../components/Headings';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Badge from '@material-ui/core/Badge';
+import MailIcon from '@material-ui/icons/Mail';
+import { withStyles } from '@material-ui/core/styles';
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    backgroundColor: '#00ab55',
+  },
+}))(Badge);
 
 const Nav = () => {
+  const [search, setSearch] = useState('');
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
-    <nav className='nav'>
-      <div className='nav__content container'>
-        <Link to='/' className='nav__logo'>
-          <img className='nav__logo-img' src={logo} alt='nftchain logo' />
-        </Link>
-        <ul className='nav__list'>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/litepaper'>
-              Litepaper
-            </Link>
-          </li>
-          <li className='nav__list-item'>
-            <a className='nav__link' href='#'>
-              About
-            </a>
-          </li>
-          <li className='nav__list-item'>
-            <a className='nav__link' href='#features'>
-              Features
-            </a>
-          </li>
-          <li className='nav__list-item'>
-            <a className='nav__link' href='#roadmap'>
-              Roadmap
-            </a>
-          </li>
-          <li className='nav__list-item'>
-            <Link className='nav__link' to='/marketplace'>
-              Buy now
-            </Link>
-          </li>
-        </ul>
+    <nav className='nav-marketplace'>
+      <div className='nav-marketplace__left-side'>
+        <div className='nav-marketplace__logo-container'>
+          <Link to='/' className='nav-marketplace__logo-link'>
+            <img
+              className='nav-marketplace__logo-img'
+              src={logo}
+              alt='NFTChain logo'
+            />
+            <H3 text={'NFTChain'} />
+          </Link>
+        </div>
+        <div className='nav-marketplace__discover-how'>
+          <Link to='/' className='nav-marketplace__link'>
+            <H6 text={'Discover'} style={{ color: '#101b32' }} />
+          </Link>
+          <Link to='/' className='nav-marketplace__link'>
+            <H6 text={'How it works'} />
+          </Link>
+        </div>
+      </div>
+      <div className='nav-marketplace__right-side'>
+        <OutlinedInput
+          id='outlined-adornment-password'
+          type={'text'}
+          value={search}
+          onChange={handleSearchChange}
+          endAdornment={
+            <InputAdornment position='end'>
+              <SearchIcon aria-label='toggle password visibility' edge='end' />
+            </InputAdornment>
+          }
+          labelWidth={70}
+        />
+        <StyledBadge variant='dot'>
+          <MailIcon />
+        </StyledBadge>
       </div>
     </nav>
   );
