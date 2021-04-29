a097eb6cdcf70e51479c88031e6c7ce7a3bb12d7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2021-04-29 14:59:02 +0200

    delete unused code in nft card

--- src/views/Marketplace/NFTInfoPage/components/ArtistAndOwner.js
+++ src/views/Marketplace/NFTInfoPage/components/ArtistAndOwner.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const ArtistAndOwner = () => {
+const ArtistAndOwner = ({ artist, owner }) => {
   return (
     <div className='artist-and-owner'>
       <div className='artist-and-owner__container'>
         <div className='artist-and-owner__card'>
           <div className='artist-and-owner__avatar' />
           <div className='artist-and-owner__user'>
-            <span className='artist-and-owner__title'>Owned by</span>
-            <span className='artist-and-owner__code'>2304RC</span>
+            <span className='artist-and-owner__title'>Owner</span>
+            <span className='artist-and-owner__code'>{owner.slice(0.4)}</span>
           </div>
         </div>
       </div>
@@ -16,8 +16,8 @@ const ArtistAndOwner = () => {
         <div className='artist-and-owner__card'>
           <div className='artist-and-owner__avatar' />
           <div className='artist-and-owner__user'>
-            <span className='artist-and-owner__title'>Owned by</span>
-            <span className='artist-and-owner__code'>2304RC</span>
+            <span className='artist-and-owner__title'>Artist</span>
+            <span className='artist-and-owner__code'>{artist.slice(0, 4)}</span>
           </div>
         </div>
       </div>
