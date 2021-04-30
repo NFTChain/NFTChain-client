0681449f76386778a0c6c9807f12d0d85d9dffd5
Liam Sutton <devliam90@gmail.com>
2021-04-30 15:52:20 +0100

    fix pagination overflow

--- src/views/Layout/Layout.js
+++ src/views/Layout/Layout.js
@@ -3,7 +3,7 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className='layout'>
       <Nav />
       {children}
       <Footer />
