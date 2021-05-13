d90cdfb370801f2302697e77ef0b625ce193f679
Liam Sutton <devliam90@gmail.com>
2021-05-13 21:13:00 +0100

    marketplace styling

--- src/views/Layout/components/Footer/Footer.js
+++ src/views/Layout/components/Footer/Footer.js
@@ -7,14 +7,6 @@ const Footer = () => {
     <div className='footer-marketplace'>
       <div className='footer-marketplace__content container'>
         <div className='footer-marketplace__row'>
-          <div className='footer-marketplace__text'>
-            <h3 className='footer-marketplace__title'>NFTChain</h3>
-          </div>
-          <div className='footer-marketplace__socials-box'>
-            <Socials />
-          </div>
-        </div>
-        <div className='footer-marketplace__row'>
           <div className='footer-marketplace__copyright'>
             <p className='footer-marketplace__copyright-text'>
               &copy; 2021
@@ -26,37 +18,8 @@ const Footer = () => {
               | All rights reserved
             </p>
           </div>
-          <div className='footer-marketplace__nav'>
-            <ul className='footer-marketplace__nav-list'>
-              {/* <li className='footer-marketplace__nav-item'>
-                <Link to='/litepaper' className='footer-marketplace__nav-link'>
-                  litepaper
-                </Link>
-              </li> */}
-              <li className='footer-marketplace__nav-item'>
-                <a href='/marketplace' className='footer-marketplace__nav-link'>
-                  marketplace
-                </a>
-              </li>
-              <li className='footer-marketplace__nav-item'>
-                <a href='/#features' className='footer-marketplace__nav-link'>
-                  features
-                </a>
-              </li>
-              <li className='footer-marketplace__nav-item'>
-                <a href='/#roadmap' className='footer-marketplace__nav-link'>
-                  roadmap
-                </a>
-              </li>
-              <li className='footer-marketplace__nav-item'>
-                <Link
-                  to='/marketplace'
-                  className='footer-marketplace__nav-link'
-                >
-                  buy now
-                </Link>
-              </li>
-            </ul>
+          <div className='footer-marketplace__socials-box'>
+            <Socials />
           </div>
         </div>
       </div>
